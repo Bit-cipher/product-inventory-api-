1b196c053b3ecaf4a7ee1848a2e26eea0c4fbfac
Madandola james <jamesmadandola@gmail.com>
2026-06-03 05:09:17 +0100

    add GET /api/products/:id route with 404 handling

--- server.js
+++ server.js
@@ -10,3 +10,16 @@ app.use(express.json());
 
 //Products Inventory
 const products = require("./data/inventory");
+
+//API to get specific product by ID
+app.get('/api/products/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+     const product = products.find(p => p.id === id );
+
+     if (!product) {
+        return res.status(404).json({ message: `Product with ID ${id} not found` });
+     }
+     res.status(200).json(product);
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
