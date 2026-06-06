2f63d917e71b596368b1aa210cf57f54c21e422f
BariwuJhunnior <bariwunyankpani@gmail.com>
2026-06-06 15:03:44 -0800

    feature: implemented error middleware handler and also edited the route for patching products

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const errorHandler = require("./middleware/errorHandler");
 
 const PORT = process.env.PORT || 8000;
 
@@ -12,40 +13,45 @@ app.use(express.json());
 const products = require("./data/inventory");
 
 //API to get specific product by ID
-app.get('/api/products/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-     const product = products.find(p => p.id === id );
+app.get("/api/products/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const product = products.find((p) => p.id === id);
 
-     if (!product) {
-        return res.status(404).json({ message: `Product with ID ${id} not found` });
-     }
-     res.status(200).json(product);
+  if (!product) {
+    return res.status(404).json({ message: `Product with ID ${id} not found` });
+  }
+  res.status(200).json(product);
 });
 
 //UPDATE Endpoint
-app.put('/api/products/:id', (req, res) => {
-   const id = parseInt(req.params.id);
+app.patch("/api/products/:id", (req, res) => {
+  const id = parseInt(req.params.id);
 
-   const product = products.find((product) => product.id ===id);
+  const product = products.find((product) => product.id === id);
 
-   if (!product){
-      return res.status(404).json({
-         message: 'Product not found',
-      });
-   }
+  if (!product) {
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  }
 
-   const {name, description, price, stock, category} = req.body;
-   
-   if (name != undefined) product.name = name;
-   if (description != undefined) product.description = description
-   if (price != undefined) product.price = price;
-   if (stock != undefined) product.stock = stock;
-   if (category != undefined) product.category = category;
+  const { name, description, price, stock, category } = req.body;
 
-   res.status (200).json({
-      message: "Product updated successfully",
-      product,
-   });
+  if (name != undefined) product.name = name;
+  if (description != undefined) product.description = description;
+  if (price != undefined) product.price = price;
+  if (stock != undefined) product.stock = stock;
+  if (category != undefined) product.category = category;
+
+  Object.assign(product, req.body);
+
+  res.status(200).json({
+    message: "Product updated successfully",
+    product,
+  });
 });
+
+//Error Handler Middleware
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
